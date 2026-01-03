90b16d18e894396f1b1d11abc8b00580b6ad7275
hanabisparkle <qing2005.el@gmail.com>
2026-01-03 17:05:54 +0700

    Commit No. 3 UI Element and Theme

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -28,6 +28,8 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import com.example.lab_week_09.ui.theme.OnBackgroundItemText
+import com.example.lab_week_09.ui.theme.PrimaryTextButton
 
 //Previously we extend AppCompatActivity,
 //now we extend ComponentActivity
@@ -98,9 +100,9 @@ fun HomeContent(
                 modifier = Modifier.padding(16.dp).fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = stringResource(
-                    id = R.string.enter_item)
-                )
+                OnBackgroundItemText(text = stringResource(
+                    id = R.string.enter_item
+                ))
                 TextField(
                     value = inputField.name,
                     keyboardOptions = KeyboardOptions(
@@ -110,12 +112,10 @@ fun HomeContent(
                         onInputValueChange(it)
                     }
                 )
-                Button(onClick = {
+                PrimaryTextButton(text = stringResource(
+                    id = R.string.button_click)
+                ) {
                     onButtonClick()
-                }) {
-                    Text(text = stringResource(
-                        id = R.string.button_click)
-                    )
                 }
             }
         }
@@ -124,7 +124,7 @@ fun HomeContent(
             Column(modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                Text(text = item.name)
+                OnBackgroundItemText(text = item.name)
             }
         }
     }
